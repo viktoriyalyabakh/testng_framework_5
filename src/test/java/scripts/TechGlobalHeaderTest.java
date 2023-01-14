24f17b26a12704fffbbd0d98da0d72bac3692bed
TechGlobalSchool <tgschool01@gmail.com>
2023-01-14 15:52:02 -0600

    added utils

--- src/test/java/scripts/TechGlobalHeaderTest.java
+++ src/test/java/scripts/TechGlobalHeaderTest.java
@@ -1,29 +1,26 @@
 package scripts;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TechGlobalHeaderTest extends TechGlobalBase {
 
-    @Test
+    @Test(priority = 1)
     public void validateTechGlobalHeaderDropdown() {
-        WebElement headerDropdown = driver.findElement(By.id("dropdown-button"));
-
-        Assert.assertTrue(headerDropdown.isDisplayed());
-        Assert.assertEquals(headerDropdown.getText(), "Practices");
+        Assert.assertTrue(techGlobalBasePage.headerDropdown.isDisplayed());
+        Assert.assertEquals(techGlobalBasePage.headerDropdown.getText(), "Practices");
     }
 
-    @Test
+    @Test(priority = 2)
     public void validateTechGlobalHeaderDropdownOptions(){
-        /*
-        Go to TechGlobal Training website
-        Click on "Practices" dropdown
-        Validate options below are displayed and enabled and their text is as below
-        Frontend Testing
-        Backend Testing
-        Java Exercises
-         */
+        techGlobalBasePage.headerDropdown.click();
+
+        String[] optionTexts = {"Frontend Testing", "Backend Testing", "Java Exercises"};
+
+        for (int i = 0; i < techGlobalBasePage.headerDropdownOptions.size(); i++) {
+            Assert.assertTrue(techGlobalBasePage.headerDropdownOptions.get(i).isDisplayed());
+            Assert.assertTrue(techGlobalBasePage.headerDropdownOptions.get(i).isEnabled());
+            Assert.assertEquals(techGlobalBasePage.headerDropdownOptions.get(i).getText(), optionTexts[i]);
+        }
     }
 }
--- src/test/java/scripts/TechGlobalLogoTest.java
+++ src/test/java/scripts/TechGlobalLogoTest.java
@@ -1,16 +1,13 @@
 package scripts;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TechGlobalLogoTest extends TechGlobalBase{
+public class TechGlobalLogoTest extends TechGlobalBase {
 
     @Test
     public void validateTechGlobalLogo() {
-        WebElement logo = driver.findElement(By.id("logo"));
-        Assert.assertTrue(logo.isDisplayed());
+        Assert.assertTrue(techGlobalBasePage.logo.isDisplayed());
     }
 
 }
